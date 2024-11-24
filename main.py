efd85b9ef206fe04a1834a8f701ded0827a85d38
BLR <bruff@Macky-Max.local>
2024-11-23 23:27:05 -0500

    this feature Compiles headers and data to a CSV file.

--- main.py
+++ main.py
@@ -13,12 +13,27 @@ def get_next_ten_numbers(start_num):
     """
     
 def list_to_comma_delimited_string(string_list):
-    return ', '.join(string_list)
-    
+    return ', ' .join(string_list)
+
     """
     Convert a list of strings to a comma-delimited string.
     Args:
         string_list (list): A list of strings.
     Returns:
         str: A comma-delimited string.
+    """
+
+def write_to_csv(headers, data, filename):
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(headers)  
+        writer.writerow(data)     
+        
+    """
+    Write headers and data to a CSV file.
+    Args:
+        Headers: Creates headers for the CSV file.
+        Data: Stores the data in CSV file.
+    Result:
+        Creates the CSV file
     """
